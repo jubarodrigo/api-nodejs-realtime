292c871e7307df7c2852a656443b48746f220026
Juba Rodrigo <rgjuba@gmail.com>
2019-02-07 05:53:54 -0200

    tweet and like crud

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-	
+
 require('dotenv').load();
 
 const app = express();
 
 mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true });
 
+app.use(express.json());
 app.use(require('./routes'));
 
 app.listen(3000, () => {
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,13 @@ const express = require('express');
 
 const routes = express.Router();
 
-routes.get('/', (req,res) =>{
-    return res.send('Hello World')
-})
+const TweetController = require('./controllers/TweetController');
+const LikeController = require('./controllers/LikeController');
+
+routes.get('/tweets', TweetController.index);
+routes.post('/tweet', TweetController.store);
+
+routes.post('/likes/:id', LikeController.store);
 
 
 module.exports = routes;
